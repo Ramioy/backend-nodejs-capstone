dd29a78b36bac72a35ec648b9e74c46e5320a7a9
Yoimar <ymoreno@anutibara.co>
2025-02-12 16:26:07 -0500

    Create main.yml

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -3,8 +3,8 @@ const MongoClient = require('mongodb').MongoClient
 const fs = require('fs')
 
 // MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`
-let filename = `${__dirname}/secondChanceItems.json`
+const url = `${process.env.MONGO_URL}`
+const filename = path.join(__dirname, 'secondChanceItems.json')
 const dbName = 'secondChance'
 const collectionName = 'secondChanceItems'
 
@@ -13,27 +13,28 @@ const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
 
 // connect to database and insert data into the collection
 async function loadData() {
+
   const client = new MongoClient(url)
 
   try {
     // Connect to the MongoDB client
     await client.connect()
-    console.log("Connected successfully to server")
+    console.log('Connected successfully to server')
 
     // database will be created if it does not exist
     const db = client.db(dbName)
 
     // collection will be created if it does not exist
     const collection = db.collection(collectionName)
-    let cursor = await collection.find({})
-    let documents = await cursor.toArray()
+    const cursor = await collection.find({})
+    const documents = await cursor.toArray()
 
     if (documents.length === 0) {
       // Insert data into the collection
       const insertResult = await collection.insertMany(data)
       console.log('Inserted documents:', insertResult.insertedCount)
     } else {
-      console.log("Items already exists in DB")
+      console.log('Items already exists in DB')
     }
   } catch (err) {
     console.error(err)
